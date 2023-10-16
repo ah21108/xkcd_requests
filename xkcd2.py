59bf6c05604c3dc101509ee163cc81cb03697120
ah21108 <armin.heravi@gmail.com>
2023-10-15 22:33:42 -0400

    mult thread with conc futures; save text to dictionary

---
+++ xkcd2.py
@@ -0,0 +1,147 @@
+import requests
+import os
+import time
+import concurrent.futures
+
+
+num_threads = 16
+start = time.time()
+
+content = {}
+
+def checkComics(number):
+    website = 'https://xkcd.com/' + str(number) + '/info.0.json'
+    response = requests.get(website)
+    
+    #checks to see if a valid site--gets over the 404 problem
+    if response.status_code == requests.codes.ok:  
+        # print(f'{number}: OK')
+        data = response.json()
+        r = requests.get(data['img'])
+        filename = 'images/' + str(number) +'.png'
+        
+        content[str(number)]=[data['safe_title'], data['alt']]
+
+        with open(filename, 'wb')as f:
+            f.write(r.content)
+    else: print(f'{number}: FAILED')
+    # return response
+        
+        
+#make directory to store images:
+if not os.path.isdir('images'):
+    os.mkdir('images')
+
+
+'''This determines the latest xkcd cartoon to create the for loop to cover all'''
+latest_comic = 'https://xkcd.com/info.0.json'
+temp = requests.get(latest_comic)
+# temp_data = temp.json()
+last_comic_number = int(temp.json()['num'])
+list_of_comics = []
+for x in range(last_comic_number):
+    list_of_comics.append(x+1)
+    
+
+with concurrent.futures.ThreadPoolExecutor(max_workers = num_threads) as executor:  
+    checkSite = {executor.submit(checkComics, comic): comic for comic in list_of_comics}
+# z= len(list_of_comics) 
+
+# lists = np.array_split(list_of_comics, num_threads)
+
+# bkpts = []
+# for a in range(num_threads):
+#     bkpts.append(int(z * ( (a+1) / num_threads) ) )
+# bkpts = [int(z//4),int(z//2),int(z * 0.75)]
+
+#generate lists
+# for thread in range(8):
+    
+#     check_comic_thread_ + str(thread) = threading.Thread(target=checkComics, args = lists[thread],) 
+   
+# list1 = list_of_comics[:(bkpts[0])+1]
+# list2 = list_of_comics[(bkpts[0]): (bkpts[1])+1]    
+# list3 = list_of_comics[(bkpts[1]): (bkpts[2])+1]
+# list4 = list_of_comics[(bkpts[2]):]     
+print(f'Start at {start}')   
+# checkComics(list_of_comics)
+# check_comic_thread = threading.Thread(target=checkComics, args=(list1,))
+# check_comic_thread2 = threading.Thread(target=checkComics, args=(list2,))
+# check_comic_thread3 = threading.Thread(target=checkComics, args=(list3,))
+# check_comic_thread4 = threading.Thread(target=checkComics, args=(list4,))
+
+# check_comic_thread.start()
+# check_comic_thread2.start()
+# check_comic_thread3.start()
+# check_comic_thread4.start()
+
+# check_comic_thread.join()
+# check_comic_thread2.join()
+# check_comic_thread3.join()
+# check_comic_thread4.join()
+end = time.time()
+
+print(f'DONE. {end-start} seconds. ')
+# #iterate over comics
+# #iterate over comics  **NEED TO CHANGE to cover full range**
+# for x in range(1, last_comic_number+1):
+#     website = 'https://xkcd.com/' + str(x) + '/info.0.json'
+#     response = requests.get(website)
+    
+#     #checks to see if a valid site--gets over the 404 problem
+#     if response.status_code == requests.codes.ok:  
+#         print(f'{x}: OK')
+#         data = response.json()
+#     else: print(f'{x}: FAILED')
+        
+        #gets rid of special characters title name to prevent save errors
+        # alphanumeric = ""
+        # for character in data['safe_title']:
+        #     if character.isalnum():
+        #         alphanumeric += character
+        
+        # #saves file
+        # filename = 'images/' + str(x)  +'.png'
+        # r = requests.get(data['img'])
+
+        # with open(filename, 'wb')as f:
+        #     f.write(r.content) 
+
+#         # add image into document
+#         #imageName = "images/" + "409_ElectricSkateboardDoubleComic.png"
+#         builder.insert_image(filename)  
+
+#         """insert hovertext below image"""
+#         # create font
+#         font = builder.font
+#         font.size = 16
+#         font.bold = True
+#         font.name = "Arial"
+#         #font.underline = aw.Underline.DASH
+
+#         # set paragraph formatting
+#         paragraphFormat = builder.paragraph_format
+#         paragraphFormat.first_line_indent = 8
+#         paragraphFormat.alignment = aw.ParagraphAlignment.JUSTIFY
+#         paragraphFormat.keep_together = True
+
+#         # add text
+#         builder.writeln(data['alt'])  
+     
+#         '''**NEED TO WORK INTO DOC)this writes alt text to text file, with line numbered by comic'''
+#         hover_text = data['alt']
+#         number = data['num']
+
+
+#         with open("images/alt-text.txt", 'a')as f:  #if printing in book this can be skipped
+#             f.write(str(number) + ': ' + hover_text + "\n")
+#             f.write("\n")
+      
+
+
+
+
+# # save document
+# doc.save("out.docx")
+# '''deletes directory used to store images during process'''  
+# shutil.rmtree('images')
